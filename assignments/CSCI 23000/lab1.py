d5f365c84dc773c637834057a9e0a94eedd818f6
lavyyy <ilavywavy@gmail.com>
2023-08-22 17:04:57 -0400

    add lab1

---
+++ assignments/CSCI 23000/lab1.py
@@ -0,0 +1,13 @@
+"""
+Name: Cayden Young
+Course: CSCI 23000
+Assignment: Lab 1
+Date: 8/25/2021
+"""
+
+# This is a single line comment!
+
+print('Yo! My name is Cayden Young')
+print('I\'m a computer science major at IUPUI / Purdue University Indianapolis')
+print('My favorite animal is a fox 🦊')
+print('You can reach me at my personal email: caydenyoung2005@gmail.com')
